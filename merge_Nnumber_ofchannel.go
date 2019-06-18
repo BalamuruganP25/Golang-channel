b13e7531f1d7680b8af731a32c48927a755a1db8
BalamuruganP25 <50692411+BalamuruganP25@users.noreply.github.com>
2019-06-18 15:41:40 +0000

    Add files via upload

---
+++ merge_Nnumber_ofchannel.go
@@ -0,0 +1,68 @@
+package main
+import(
+
+ "fmt"
+ "time"
+ "sync"
+
+)
+
+func main(){
+
+	channalone   := aschan(1,2,3,4,5,6,7,8,9)
+    channaltwo   := aschan(10,11,12,13,14,15,16,17,18,19)
+    channalthree := aschan(20,21,22,23,24,25,26,27,28,29)
+
+    for v:= range merge(a,b,c){
+
+    	fmt.Println(v)
+
+    }
+ 
+}
+
+
+func merge(chans ... <-chan int) <-chan int{
+
+  out := make(chan int) 
+
+	  go func(){
+	  	var wg sync.WaitGroup 
+	  	wg.Add(len(chans))
+	  	for _,c := range chans {
+	  	   go func( c <- chan int){
+	  	   	 for v:= range c{
+
+	  	   		out <-v 
+	  	   	}
+
+	  	   	wg.Done()
+
+	  	}(c)
+
+ }
+	wg.Wait()
+	close(out)
+
+}()
+
+return out
+
+}
+
+
+func aschan(vs ...int) <-chan int{
+	c:= make(chan int)
+	go func(){
+   		for _,v := range vs{
+			c <- v
+  			time.Sleep(2 * time.Second)
+
+   		}
+
+   		close(c)
+
+	}()
+
+  return c
+}
